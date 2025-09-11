c12db6e7ff656e9e6844ee152be286f6f5f217c5
Test <testgyaan12@gmail.com>
2025-09-11 05:08:28 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -11,7 +11,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent, SheetHeader } from "@/components/ui/sheet"
+import { Sheet, SheetContent } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -710,5 +710,3 @@ export {
   SidebarTrigger,
   useSidebar,
 }
-
-    
